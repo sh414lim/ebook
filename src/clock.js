6b2035f442ac6ecb6c58acb171094f7edd1876fe
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-05-17 11:34:55 +0900

    .

---
+++ src/clock.js
@@ -0,0 +1,16 @@
+const data=document.querySelector(".lock_js")
+const clock=data.querySelector("h1");
+
+
+    function getTime(){
+        const 
+    }
+
+
+    function init(){
+
+    }
+
+    init();
+
+
