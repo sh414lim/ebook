5c394a228753c096089eddc6603f27f83980a861
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-05-18 16:39:58 +0900

    .

--- src/Form/Artist.js
+++ src/Form/Artist.js
@@ -2,16 +2,23 @@ import React from "react";
 import styled from "styled-components";
 
 const Container=styled.div`
-
+border-radius:30px;
+box-shadow:6px 6px rgba(20,20,20,0.3), -1em 0 .4em rgba(20,20,20,0.5);
+background-color:white;
+width:90vw;
+height:1000vh;
+margin-left:80px;
+margin-top:30px;
 `;
 
 const Title=styled.span`
-  display:flex;
+ display:flex;
   justify-content:center;
   align-items:center;
   font-size:30px;
   font-weight:600;
   margin-top:20px;
+  padding:20px;
 `;
 
 const Item=styled.ul`
--- src/Form/Song.js
+++ src/Form/Song.js
@@ -2,7 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 const Container=styled.div`
-
+border-radius:30px;
+box-shadow:6px 6px rgba(20,20,20,0.3), -1em 0 .4em rgba(20,20,20,0.5);
+background-color:white;
+width:90vw;
+height:1000vh;
+margin-left:80px;
+margin-top:30px;
 `;
 
 const Title=styled.span`
@@ -11,18 +17,21 @@ const Title=styled.span`
   align-items:center;
   font-size:30px;
   font-weight:600;
+  padding:20px;
+  
 `;
 
 const Item=styled.ul`
     display:block;
     margin-left:50px;
+    margin-top:30px;
     `;
 
 const List=styled.li`
     display: grid;
-    grid-template-columns:repeat(2,1fr);
-    grid-template-rows:repeat(2,1fr);
-    grid-gap:30px;
+    grid-template-columns:repeat(2,2fr);
+    grid-template-rows:repeat(2,2fr);
+    grid-gap:50px;
     margin-top:50px;
     font-weight:600;
 `;
