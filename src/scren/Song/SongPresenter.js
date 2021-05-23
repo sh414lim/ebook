9034de02f336dba7cf3ecbba54957e3838239e65
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-05-23 23:41:54 +0900

    css

--- src/scren/Song/SongPresenter.js
+++ src/scren/Song/SongPresenter.js
@@ -4,6 +4,25 @@ import styled from "styled-components";
 
 const Container =styled.div`
 padding: 10px;
+
+`;
+
+const P=styled.p`
+font-size: 13PX;
+color:rgba(0,0,0,0.5);
+&:hover{
+font-size: 15px;
+
+}
+`;
+
+const H3=styled.h3`
+font-size: 17px;
+padding: 10px;
+`;
+
+const Span =styled.span`
+font-size: 20px;
 `;
 
 
@@ -12,9 +31,12 @@ const SongPresenter =({TopTrack,loading})=>loading ? null :(
         {TopTrack && TopTrack.length > 0 &&(
             <Form title="Top Song">
                 {TopTrack.map(song=>
-                <span>
-                    {song.name}
-                </span>
+                <Span>
+                    #Title:{song.name}<br/>
+                   <H3>#Artist:{song.artist.name}</H3>
+                    <P>playcount:{song.playcount}</P>
+                    <P>listeners:{song.listeners}</P>
+                </Span>
                 )}
             </Form>
             
